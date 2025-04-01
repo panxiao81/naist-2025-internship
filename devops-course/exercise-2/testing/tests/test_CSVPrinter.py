154537284e06de817ecde84889b16547060f0580
panxiao81 <pan-xiao@live.cn>
2025-04-01 13:38:30 +0900

    Devops Course/Exercise 2-3: add special methods

--- devops-course/exercise-2/testing/tests/test_CSVPrinter.py
+++ devops-course/exercise-2/testing/tests/test_CSVPrinter.py
@@ -2,6 +2,12 @@ import unittest
 from csvprinter.CSVPrinter import CSVPrinter
 
 
+def setUpModule():
+    print("Running setUpModule")
+
+def tearDownModule():
+    print("Running setDownModule")
+
 class TestCSVPrinter(unittest.TestCase):
 
     def test_TestMethod1(self):
@@ -18,3 +24,21 @@ class TestCSVPrinter(unittest.TestCase):
         printer = CSVPrinter("a-file-does-not-exist.csv")
         with self.assertRaises(FileNotFoundError):
             printer.read()
+
+    @classmethod
+    def setUpClass(cls):
+        print("Running setUpClass")
+        return super().setUpClass()
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("Running tearDownClass")
+        return super().tearDownClass()
+    
+    def setUp(self):
+        print("Running setUp")
+        return super().setUp()
+    
+    def tearDown(self):
+        print("Running tearDown")
+        return super().tearDown()
